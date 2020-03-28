e0d05d6d34711979aea137af766cc024709cf81c
tpmccallum <mistermac2008@gmail.com>
2020-03-28 14:31:43 +1000

    Print statements

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub extern "C" fn store_data(_key: i64, _value: *const c_char) {
         CStr::from_ptr(_value)
     };
     let _value_as_string = c_str.to_str().unwrap();
-    println!("value_as_string: {:?}", _value_as_string);
+    //println!("value_as_string: {:?}", _value_as_string);
     println!("Storing data");
     let path = "/media/nvme/ssvm_database";
     println!("Database path: {:?}", path);
@@ -21,7 +21,7 @@ pub extern "C" fn store_data(_key: i64, _value: *const c_char) {
     let mut opts = Options::default();
     opts.increase_parallelism(3);
     opts.create_if_missing(true);
-    println!("Database options are set");
+    //println!("Database options are set");
     db.put(_key.to_string(), _value_as_string).unwrap();
     println!("Item added to database");
 }
@@ -30,15 +30,15 @@ pub extern "C" fn store_data(_key: i64, _value: *const c_char) {
 pub extern "C" fn load_data(_key: i64) -> *mut c_char {
     println!("Loading data");
     let path = "/media/nvme/ssvm_database";
-    println!("Database path: {:?}", path);
+    //println!("Database path: {:?}", path);
     let db = DB::open_default(path).unwrap();
-    println!("Database instance: {:?}", db);
+    //println!("Database instance: {:?}", db);
     let mut opts = Options::default();
     opts.increase_parallelism(3);
-    println!("Database options are set");
+    //println!("Database options are set");
     let db_value_as_vec = db.get(_key.to_string()).unwrap().unwrap();
     let db_value_as_cstring = CString::new(db_value_as_vec).unwrap();
-    println!("Value as CString: {:?}", db_value_as_cstring);
+    //println!("Value as CString: {:?}", db_value_as_cstring);
     db_value_as_cstring.into_raw()
 }
 
