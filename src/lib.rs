8ceb4f6a1a0c90d1319f56f254f6216d3b4eddef
tpmccallum <mistermac2008@gmail.com>
2020-04-10 17:41:32 +1000

    Code up the ability to store_bytes and load_bytes (no strings); just pointers by reference as apposed to passing anything by val

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,49 @@
 extern crate libc;
 extern crate cc;
-use libc::c_char;
+use std::slice;
+use libc::{c_char};
 use rocksdb::{Options, DB};
 use std::ffi::{CStr, CString};
 
+//pub extern "C" fn store_bytes(_key: *const c_char, _buffer_address: *const c_char, _length: size_t){
+#[no_mangle]
+pub extern "C" fn store_bytes(_key: *const c_char, _value: *const c_char) {
+    let c_str_key = unsafe {
+        assert!(!_key.is_null());
+
+        CStr::from_ptr(_key)
+    };
+    let c_str_value = unsafe {
+        assert!(!_value.is_null());
+
+        CStr::from_ptr(_value)
+    };
+    println!("Storing data, please wait ...");
+    let path = "/media/nvme/ssvm_database";
+    println!("Database path: {:?}", path);
+    let db = DB::open_default(path).unwrap();
+    println!("Database instance: {:?}", db);
+    db.put(c_str_key.to_bytes(), c_str_value.to_bytes()).unwrap();
+    println!("Item added to database"); 
+}
+
+#[no_mangle]
+pub extern "C" fn load_bytes(_key: *const c_char) -> *mut c_char {
+    let c_str_key = unsafe {
+        assert!(!_key.is_null());
+
+        CStr::from_ptr(_key)
+    };
+    println!("Loading data, please wait ...");
+    let path = "/media/nvme/ssvm_database";
+    println!("Database path: {:?}", path);  
+    let db = DB::open_default(path).unwrap();
+    println!("Database instance: {:?}", db);
+    CString::new(db.get(c_str_key.to_bytes()).unwrap().unwrap()).unwrap().into_raw()
+}
+
 #[no_mangle]
 pub extern "C" fn store_data(_key: *const c_char, _value: *const c_char) {
-//pub extern "C" fn store_data() {
 	println!("Storing data, please wait ...");
     let c_str = unsafe {
         assert!(!_value.is_null());
@@ -43,7 +80,7 @@ pub extern "C" fn load_data(_key: *const c_char) -> *mut c_char {
     };
     let _key_as_string = c_str_key.to_str().unwrap();
     let path = "/media/nvme/ssvm_database";
-    //println!("Database path: {:?}", path);
+    //println!("Database path: {:?}", path);*  
     let db = DB::open_default(path).unwrap();
     //println!("Database instance: {:?}", db);
     let mut opts = Options::default();
--- src/test_lib.h
+++ src/test_lib.h
@@ -9,6 +9,10 @@ void store_data(const char *key, const char *value);
 char * load_data(const char *key);
 void free_pointer(char *);
 
+void store_bytes(const char *key, const char *buffer_address, const size_t *length);
+char * load_bytes(const char *key);
+void free_byte_pointer(char *);
+
 #ifdef __cplusplus
 }
 #endif
