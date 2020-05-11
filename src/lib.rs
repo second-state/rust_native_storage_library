e155add8761f175fb0c04593ee22c1d955219e3b
Timothy McCallum <tpmccallum@users.noreply.github.com>
2020-05-11 14:02:30 +1000

    Remove outdated code that was work-in-progress
    Remove the older functions which shared strings etc.
    Also remove unused dependencies.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 use libc::size_t;
 use rocksdb::DB;
-use std::alloc::{dealloc, Layout};
 use std::convert::TryInto;
 use std::os::raw::c_char;
-use std::ptr;
 use std::slice;
 
 #[no_mangle]
@@ -67,109 +65,3 @@ pub extern "C" fn free_byte_array_pointer(value_array_pointer: *mut c_char, valu
         Box::from_raw(s);
     }
 }
-
-// The code below worked really well
-// i.e. store/load (using CStr as string) and store/load (using CStr as bytes)
-// However these original functions will be excluded from the API
-// Reason being any use of CStr means that the data is not allowed to have \0 \0x00 nul
-// We need to accomodate these values as valid input. The DB will put and get arbitrary byte arrays so we need to ensure that this API does not offer less than that
-/*
-#[no_mangle]
-pub extern "C" fn store_bytes(_key: *const c_char, _value: *const c_char) {
-    let c_str_key = unsafe {
-        assert!(!_key.is_null());
-
-        CStr::from_ptr(_key)
-    };
-    let c_str_value = unsafe {
-        assert!(!_value.is_null());
-
-        CStr::from_ptr(_value)
-    };
-    println!("Storing data, please wait ...");
-    let path = "/media/nvme/ssvm_database";
-    println!("Database path: {:?}", path);
-    let db = DB::open_default(path).unwrap();
-    println!("Database instance: {:?}", db);
-    db.put(c_str_key.to_bytes(), c_str_value.to_bytes())
-        .unwrap();
-    println!("Item added to database");
-}
-
-#[no_mangle]
-pub extern "C" fn load_bytes(_key: *const c_char) -> *mut c_char {
-    let c_str_key = unsafe {
-        assert!(!_key.is_null());
-
-        CStr::from_ptr(_key)
-    };
-    println!("Loading data, please wait ...");
-    let path = "/media/nvme/ssvm_database";
-    println!("Database path: {:?}", path);
-    let db = DB::open_default(path).unwrap();
-    println!("Database instance: {:?}", db);
-    CString::new(db.get(c_str_key.to_bytes()).unwrap().unwrap())
-        .unwrap()
-        .into_raw()
-}
-
-#[no_mangle]
-pub extern "C" fn store_data(_key: *const c_char, _value: *const c_char) {
-    println!("Storing data, please wait ...");
-    let c_str = unsafe {
-        assert!(!_value.is_null());
-
-        CStr::from_ptr(_value)
-    };
-    let _value_as_string = c_str.to_str().unwrap();
-
-    let c_str_key = unsafe {
-        assert!(!_key.is_null());
-
-        CStr::from_ptr(_key)
-    };
-    let _key_as_string = c_str_key.to_str().unwrap();
-    let path = "/media/nvme/ssvm_database";
-    println!("Database path: {:?}", path);
-    let db = DB::open_default(path).unwrap();
-    println!("Database instance: {:?}", db);
-    let mut opts = Options::default();
-    opts.increase_parallelism(3);
-    opts.create_if_missing(true);
-    //println!("Database options are set");
-    db.put(_key_as_string, _value_as_string).unwrap();
-    println!("Item added to database");
-}
-
-#[no_mangle]
-pub extern "C" fn load_data(_key: *const c_char) -> *mut c_char {
-    println!("Loading data, please wait ...");
-    let c_str_key = unsafe {
-        assert!(!_key.is_null());
-
-        CStr::from_ptr(_key)
-    };
-    let _key_as_string = c_str_key.to_str().unwrap();
-    let path = "/media/nvme/ssvm_database";
-    //println!("Database path: {:?}", path);*
-    let db = DB::open_default(path).unwrap();
-    //println!("Database instance: {:?}", db);
-    let mut opts = Options::default();
-    opts.increase_parallelism(3);
-    //println!("Database options are set");
-    let db_value_as_vec = db.get(_key_as_string).unwrap().unwrap();
-    let db_value_as_cstring = CString::new(db_value_as_vec).unwrap();
-    //println!("Value as CString: {:?}", db_value_as_cstring);
-    db_value_as_cstring.into_raw()
-}
-
-#[no_mangle]
-pub extern "C" fn free_pointer(s: *mut c_char) {
-    unsafe {
-        if s.is_null() {
-            return;
-        }
-        CString::from_raw(s)
-    };
-}
-*/
